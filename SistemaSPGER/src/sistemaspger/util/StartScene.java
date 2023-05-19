2b5e8bf18506432d30cbbeba9119feded1bcf335
CoronelTocino1 <35902658+Foforfr0@users.noreply.github.com>
2023-05-19 11:29:42 -0600

    Add files via upload

---
+++ SistemaSPGER/src/sistemaspger/util/StartScene.java
@@ -0,0 +1,22 @@
+package sistemaspger.util;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import sistemaspger.SistemaSPGER;
+
+public class StartScene {
+        public static Scene initializeScene(String ruta){
+        Scene escenaPrincipal = null;
+        try {
+            Parent vista = FXMLLoader.load(SistemaSPGER.class.getResource(""));
+            escenaPrincipal = new Scene(vista);
+        } catch (IOException ex) {
+            Logger.getLogger(StartScene.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return escenaPrincipal;
+    }  
+}
